734288b00cc4df93775ce828ea4c06c1f94ad2b2
Fernanda Dias <fernandadias9@hotmail.com>
2023-12-13 08:19:27 -0300

    Update SCRIPT_DB.sql
    limpeza do código

--- SCRIPT_DB.sql
+++ SCRIPT_DB.sql
@@ -14,7 +14,6 @@ login varchar(255),
 senha varchar(255)
 );
 
-
 create table receita (
 idreceita int not null primary key auto_increment,
 idusuario int, foreign key (idusuario) references usuario (idusuario),
@@ -22,7 +21,6 @@ descricao varchar(255),
 valor decimal(10,2),
 datareceita date
 );
-
 
 create table despesa (
 iddespesa int not null primary key auto_increment,
@@ -32,24 +30,3 @@ valor decimal(10,2),
 datavencimento date,
 datapagamento date
 );
-
-
-
-insert into usuario (nome, cpf, email, datanascimento, login, senha) 
-values ('Adriano', '01234567890', 'adriano@gmail.com', '1978-02-24', 'adriano', 'adriano');
-
-select * from usuario;
-
-select * from receita;
-select * from despesa;
-
--- create view vreceita as
--- select sum(valor) as receita, 0 as despesa, month(datareceita) as mes from receita where idusuario = 1 and year(datareceita) = 2023 group by month(datareceita);
--- create view vdespesa as
--- select 0 as receita, sum(valor) as despesa, month(datavencimento) as mes from despesa where idusuario = 1 and year(datavencimento) = 2023 group by month(datavencimento);
-
--- select ifnull(vreceita.receita, 0), ifnull(vdespesa.despesa, 0), vdespesa.mes from vreceita right join vdespesa on vreceita.mes = vdespesa.mes order by mes;
-
-select * from vdespesa;
-
-select sum(receita), sum(despesa), relatorio.mes from (select sum(valor) as receita, 0 as despesa, month(datareceita) as mes from receita where idusuario = 1  and year(datareceita) = 2023  group by month(datareceita) union select 0 as receita, sum(valor) as despesa, month(datavencimento) as mes from despesa where idusuario = 1 and year(datavencimento) = 2023 group by month(datavencimento)) relatorio group by mes order by mes;
